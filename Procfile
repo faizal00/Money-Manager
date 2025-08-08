cdfb70e2bdc36ba4e6cf696200af5313e1d2909e
Faizal <faizal@example.com>
2025-08-08 10:05:47 +0700

    Remove nixpacks, use Railway auto-detection with build script

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python main.py
+web: ./build.sh && python main.py
--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+
 # Install Python dependencies
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Install Node dependencies and build React
 npm install
